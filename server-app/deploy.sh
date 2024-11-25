1d1a046361679ab009c7426d56dec8f15ad966ac
Gabriele Santomaggio <G.santomaggio@gmail.com>
2024-11-25 09:23:27 +0100

    test url
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- server-app/deploy.sh
+++ server-app/deploy.sh
@@ -1,2 +1,2 @@
-kubectl  apply  -f   stream_cluster.yaml &&  kubectl create secret tls tls-secret --cert=test-server.pem --key=test-server-key.pem -n stream-clients-test
+kubectl  apply  -f   stream_cluster.yaml &&  kubectl create secret tls tls-secret --cert=test-server.pem --key=test-server-key.pem -n gabriele
 
